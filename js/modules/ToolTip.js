9609517afeb3f6a80332f1350591f1c516598cd7
Jpapirs <joao.pedro.apsilva@gmail.com>
2025-07-22 12:34:14 -0300

    Tooltip refatorado

--- js/modules/ToolTip.js
+++ js/modules/ToolTip.js
@@ -1,39 +1,52 @@
-export default function initToolTip() {
-  const tooltips = document.querySelectorAll("[data-tooltip]");
-
-  tooltips.forEach((item) => {
-    item.addEventListener("mouseover", onMouseOver);
-  });
-  function onMouseOver(event) {
-    const tooltipBox = criarToolTipBox(this);
-
-    onMouseMove.tooltipBox = tooltipBox;
-    this.addEventListener("mousemove", onMouseMove);
-    onMouseLeave.tooltipBox = tooltipBox;
-    onMouseLeave.element = this;
-    this.addEventListener("mouseleave", onMouseLeave);
+export default class ToolTip {
+  constructor(tooltips) {
+    this.tooltips = document.querySelectorAll(tooltips);
+    // Bind do objeto das classes dos callbacks
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseOver = this.onMouseOver.bind(this);
   }
-  const onMouseLeave = {
-    handleEvent() {
-      this.tooltipBox.remove();
-      this.element.removeEventListener("mouseleave", onMouseLeave);
-    },
-  };
 
-  const onMouseMove = {
-    handleEvent(event) {
-      this.tooltipBox.style.top = event.pageY + 20 + "px";
-      this.tooltipBox.style.left = event.pageX + 20 + "px";
-      this.element.removeEventListener("mousemove", onMouseMove);
-    },
-  };
+  onMouseMove(event) {
+    this.tooltipBox.style.top = `${event.pageY + 20}px`;
+    if (event.pageX + 240 > window.innerWidth) {
+      this.tooltipBox.style.left = `${event.pageX - 190} px`;
+    } else {
+      this.tooltipBox.style.left = `${event.pageX + 20} px`;
+    }
+  }
 
-  function criarToolTipBox(element) {
+  onMouseLeave({ currentTarget }) {
+    this.tooltipBox.remove();
+    currentTarget.removeEventListener("mouseleave", this.onMouseLeave);
+    currentTarget.removeEventListener("mousemove", this.onMouseMove);
+  }
+
+  //* Cria a tooltip Box e coloca no body
+
+  criarToolTipBox(element) {
     const tooltipBox = document.createElement("div");
     const text = element.getAttribute("aria-label");
     tooltipBox.classList.add("tooltip");
     tooltipBox.innerText = text;
     document.body.appendChild(tooltipBox);
-    return tooltipBox;
+    this.tooltipBox = tooltipBox;
+  }
+  onMouseOver({ currentTarget }) {
+    //* Cria a tooltip box e coloca em uma propriedade
+    this.criarToolTipBox(currentTarget);
+    currentTarget.addEventListener("mousemove", this.onMouseMove);
+    currentTarget.addEventListener("mouseleave", this.onMouseLeave);
+  }
+  addTooltipsEvents() {
+    this.tooltips.forEach((item) => {
+      item.addEventListener("mouseover", this.onMouseOver);
+    });
+  }
+  init() {
+    if (this.tooltips.length) {
+      this.addTooltipsEvents();
+    }
+    return this;
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import Accordion from "./modules/accordionList.js";
 import Modal from "./modules/modal.js";
-import initToolTip from "./modules/ToolTip.js";
+import ToolTip from "./modules/ToolTip.js";
 import initDropDownMenu from "./modules/MenuDropDown.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initAnimaScroll from "./modules/scroll-animation.js";
@@ -11,11 +11,17 @@ import initFetchBTC from "./modules/btcFecth.js";
 const accordion = new Accordion(".js-accordion dt");
 accordion.init();
 
-const modal = new Modal('[data-modal = "abrir" ]','[data-modal = "fechar"]','[data-modal = "container"]');
+const modal = new Modal(
+  '[data-modal = "abrir" ]',
+  '[data-modal = "fechar"]',
+  '[data-modal = "container"]'
+);
 modal.init();
 
+const tooltip = new ToolTip("[data-tooltip]");
+tooltip.init();
 
-initToolTip();
+
 initDropDownMenu();
 initMenuMobile();
 initAnimaScroll();
